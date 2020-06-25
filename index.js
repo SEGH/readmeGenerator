586180393cb1cf866ea86f32204c48199bb6460f
Susan Holland <segh@fastmail.com>
2020-06-25 18:58:24 -0400

    Functions outlined with required files

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
+
 // array of questions for user
 const questions = [
-
+    {
+        type: "input",
+        message: "What is the filename of your README?",
+        name: "fileName"
+    },
+    {
+        type: "input",
+        message: "What is the title of your README.md?",
+        name: "title"
+    }
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    console.log(fileName);
+
+    let markdown = generateMarkdown(data);
+    fs.writeFile(fileName, markdown, err => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("Sucess! Your README has been generated in " + fileName);
+    })
 }
 
 // function to initialize program
 function init() {
-
+    inquirer.prompt(questions).then(data => writeToFile(data.fileName + ".md", data));
 }
 
 // function call to initialize program
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,6 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
+  console.log(data);
   return `# ${data.title}
 
 `;
